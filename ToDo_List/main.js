2128c2f75fdf5fa44fd7306f3b00b13f4feac67b
hwonda <hwondaa@gmail.com>
2022-02-02 14:44:11 +0900

    [기능] All, Process, Done div 눌렀을 때 기능 추가한다

--- ToDo_List/main.js
+++ ToDo_List/main.js
@@ -1,7 +1,17 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add-botton");
 let taskList = [];
+let mode = "All";   // 초기값 All 넣어야 처음 넣었을 때 All 화면이 나옴
+let tabs = document.querySelectorAll(".task-tabs div")  // querySelectorAll 하면 모든 .task-tabs 아래의 div를 가져올 수 있음.
+let filterList = [];
 addButton.addEventListener("click", addTask);   // button click시 addTask 함수 실행.
+
+
+// fixed div -> html에 id 넣기
+for(let i=1;i<tabs.length;i++){
+    tabs[i].addEventListener("click",filter);
+}
+
 
 function addTask(){
     // let taskContent = taskInput.value;
@@ -17,24 +27,31 @@ function addTask(){
     taskInput.value = "";
 }
 
-// 그림을 그려주기.
+// UI update용 함수 render
 function render(){
+    let meanlessList=[];
+    if(mode == "all"){
+        meanlessList = taskList;
+    }else if(mode == "ongoing" || mode == "done"){
+        meanlessList = filterList;
+    }
+
     let resultHTML = "";
-    for(let i=0;i<taskList.length;i++){
-        if(taskList[i].isComplete == true){
+    for(let i=0;i<meanlessList.length;i++){
+        if(meanlessList[i].isComplete == true){
             resultHTML += `<div class=tasks>
-                <div class="task-done">${taskList[i].taskContent}</div>
+                <div class="task-done">${meanlessList[i].taskContent}</div>
                 <div>
-                    <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                    <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                    <button onclick="toggleComplete('${meanlessList[i].id}')">Check</button>
+                    <button onclick="deleteTask('${meanlessList[i].id}')">Delete</button>
                 </div>    
             </div>`
         }else{
             resultHTML += `<div class="tasks">
-                <div>${taskList[i].taskContent}</div>
+                <div>${meanlessList[i].taskContent}</div>
                     <div>
-                        <button onclick="toggleComplete('${taskList[i].id}')">Check</button>
-                        <button onclick="deleteTask('${taskList[i].id}')">Delete</button>
+                        <button onclick="toggleComplete('${meanlessList[i].id}')">Check</button>
+                        <button onclick="deleteTask('${meanlessList[i].id}')">Delete</button>
                     </div>
             </div>`
         }
@@ -76,4 +93,29 @@ function randomIDGenerate(){
     return '_' + Math.random().toString(36).substr(2,9);
 }
 
+function filter(event){
+    console.log("filter클릭됨", event.target.id);  // click한 component 확인 
+    filterList = [];   
+    mode = event.target.id;
+    if(mode == "all"){
+        render();
+    }else if(mode == "ongoing"){
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete == false){
+                filterList.push(taskList[i]);
+            }
+        }   
+        // taskList = filterList; 로 덮어쓰면 Original 값에 손상이 감. -> filterList를 전역변수로  
+        render();
+    }else if(mode == "done"){
+        for(let i=0;i<taskList.length;i++){
+            if(taskList[i].isComplete == true){
+                filterList.push(taskList[i])
+            }
+        }
+        render();
+    }
+    console.log(filterList);
+}
 
+    
